47ff02f42fd002a71c6596a44d6c7594d43b78c2
dongzoolee <nant0313@gmail.com>
2021-03-24 16:06:50 +0900

    Remove Unused Components

--- src/components/Category/Location/Location.js
+++ src/components/Category/Location/Location.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import Header from '../../Header/Header'
 import styles from './Location.module.scss';
 import SubCategoryTemplate from '../CategoryTemplate/SubCategoryTemplate';
 import Layout from '../../Layout';
